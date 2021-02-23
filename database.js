dc42a819b77911e0d8c119970eb69e4f7c46ec33
Guilherme Mariano Munis <guilherme.munis@pampa.compasso>
2021-02-23 16:55:53 -0300

    Funções de hashing implementação de proteção

--- database.js
+++ database.js
@@ -14,7 +14,7 @@ const USUARIOS_SCHEMA = `
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     nome VARCHAR(40) NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE,
-    senha VARCHAR(255) NOT NULL
+    senhaHash VARCHAR(255) NOT NULL
   )
   `;
 
@@ -24,8 +24,8 @@ db.serialize(() => {
   db.run(USUARIOS_SCHEMA);
 
   db.each('SELECT * FROM usuarios', (err, usuario) => {
-    console.log('Usuarios: ');
-    console.log(usuario);
+    // console.log('Usuarios: ');
+    // console.log(usuario);
   });
 });
 
